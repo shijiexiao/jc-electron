58a4508ba10a3213c0847a7777500fa32b05e788
xiaoshijie <xiaoshijie@baidu.com>
2021-02-15 20:11:04 +0800

    electron react init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <span>👍</span>
+      <div className="row">
+        <div className="col-3 bg-danger left-panel">
+          <h1>this is left</h1>
+        </div>
+        <div className="col-9 bg-primary right-panel">
+          <h1>right</h1>
+        </div>
+      </div>
     </div>
   );
 }
